be6f4c7a4957a964967f167adb676db331692ba2
Artsiom Hontar <artsiom.hontar@ihsmarkit.com>
2021-10-01 15:26:25 +0300

    feat: add db layer

---
+++ internal/app/store/store_test.go
@@ -0,0 +1,19 @@
+package store_test
+
+import (
+	"os"
+	"testing"
+)
+
+var (
+	databaseURL string
+)
+
+func TestMain(m *testing.M) {
+	databaseURL = os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = "host=localhost dbname=restapi_test sslmode=disable user=postgres password=cce16cc03cfb49c9b247fc6faff58fa7"
+	}
+
+	os.Exit(m.Run())
+}
