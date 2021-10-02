40ff7c103338a85a0eb2710bfda4c0c655d6232b
Artsiom Hontar <artsiom.hontar@ihsmarkit.com>
2021-10-02 12:04:17 +0300

    feat: refactoring 1

---
+++ internal/app/store/sqlstore/store.go
@@ -0,0 +1,27 @@
+package sqlstore
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Store struct {
+	db             *sql.DB
+	userRepository *UserRepository
+}
+
+func New(db *sql.DB) *Store {
+	return &Store{
+		db: db,
+	}
+}
+
+func (s *Store) User() *UserRepository {
+	if s.userRepository != nil {
+		return s.userRepository
+	}
+	return &UserRepository{
+		store: s,
+	}
+}
--- internal/app/store/sqlstore/userrepository_test.go
+++ internal/app/store/sqlstore/userrepository_test.go
@@ -1,38 +1,39 @@
-package store_test
+package sqlstore_test
 
 import (
 	"testing"
 
 	"github.com/ArtemGontar/web-api/internal/app/model"
-	"github.com/ArtemGontar/web-api/internal/app/store"
+	"github.com/ArtemGontar/web-api/internal/app/store/sqlstore"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
 
-	u, err := s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	s := sqlstore.New(db)
+	u := model.TestUser(t)
+	err := s.User().Create(u)
 
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
 
+	s := sqlstore.New(db)
 	email := "user@example.org"
 	_, err := s.User().FindByEmail(email)
 	assert.Error(t, err)
 
-	s.User().Create(&model.User{
-		Email: "user@example.org",
-	})
+	u := model.TestUser(t)
+	u.Email = email
+	s.User().Create(u)
 
-	u, err := s.User().FindByEmail(email)
+	u, err = s.User().FindByEmail(email)
 	assert.NoError(t, err)
 	assert.NotNil(t, u)
 }
